16060d9d9a7551d22626093551576d424481d6a0
‘nathashas1’ <nathasha36@gmail.com>
2018-09-04 15:29:17 -0700

    work on the EASY section

---
+++ aa_questions/all_users.rb
@@ -0,0 +1,202 @@
+require 'sqlite3'
+require 'singleton'
+
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class User
+  attr_accessor :fname, :lname
+  attr_reader :id
+  def self.find_by_id(id)
+    user = QuestionsDatabase.instance.execute(<<-SQL,id)
+    SELECT
+    *
+    FROM
+    users
+    WHERE
+    id = ?
+    SQL
+    return nil if user.length < 1
+    User.new(user.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
+  def self.find_by_name(fname, lname)
+    user = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+    SELECT
+    *
+    FROM
+    users
+    WHERE
+    fname = ? AND lname = ?
+
+    SQL
+    return nil if user.length < 1
+    User.new(user.first)
+  end
+
+  def authored_questions
+    Question.find_by_author_id(@id)
+  end
+
+  def authored_replies
+    Reply.find_by_user_id(@id)
+  end
+end
+
+class Question
+  attr_accessor :title, :body
+  attr_reader :id, :user_id
+  def self.find_by_id(id)
+    question = QuestionsDatabase.instance.execute(<<-SQL,id)
+    SELECT
+    *
+    FROM
+      questions
+    WHERE
+      id = ?
+    SQL
+    return nil if question.length < 1
+    Question.new(question.first)
+  end
+
+  def self.find_by_author_id(author_id)
+    question = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+    SELECT
+      *
+    FROM
+      questions
+    WHERE
+      user_id = ?
+    SQL
+    return nil if question.length < 1
+    p question
+    Question.new(question.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @user_id = options['user_id']
+  end
+end
+
+class Question_follow
+  attr_reader :id, :question_id, :user_id
+
+  def self.find_by_id(id)
+    question_follow = QuestionsDatabase.instance.execute(<<-SQL,id)
+    SELECT
+    *
+    FROM
+      question_follows
+    WHERE
+      id = ?
+    SQL
+    return nil if question_follow.length < 1
+    Question_follow.new(question_follow.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+  end
+end
+
+class Reply
+  attr_accessor :body
+  attr_reader :id, :question_id, :user_id
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM replies")
+    data.map { |datum| Reply.new(datum)}
+  end
+
+  def self.find_by_id(id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL,id)
+    SELECT
+    *
+    FROM
+      replies
+    WHERE
+      id = ?
+    SQL
+    return nil if reply.length < 1
+    Reply.new(reply.first)
+  end
+
+  def self.find_by_user_id(user_id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL,user_id)
+    SELECT
+    *
+    FROM
+      replies
+    WHERE
+      user_id = ?
+    SQL
+    return nil if reply.length < 1
+    Reply.new(reply.first)
+  end
+
+  def self.find_by_question_id(question_id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL,question_id)
+    SELECT
+    *
+    FROM
+      replies
+    WHERE
+      question_id = ?
+    SQL
+    return nil if reply.length < 1
+
+    ans =[]
+    reply.each do |el|
+      ans << Reply.new(el)
+    end
+    ans
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @body = options['body']
+    @user_id = options['user_id']
+  end
+end
+
+class Question_like
+  attr_reader :id, :question_id, :user_id
+
+  def self.find_by_id(id)
+    question_like = QuestionsDatabase.instance.execute(<<-SQL,id)
+    SELECT
+    *
+    FROM
+      question_likes
+    WHERE
+      id = ?
+    SQL
+    return nil if question_like.length < 1
+    Question_follow.new(question_like.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+  end
+end
---
+++ aa_questions/import_db.sql
@@ -0,0 +1,70 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER NOT NULL PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
+  -- FOREIGN KEY (ques_id) REFERENCES questions(id)
+);
+
+
+CREATE TABLE questions (
+  id INTEGER NOT NULL PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+CREATE TABLE question_follows (
+  id INTEGER NOT NULL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies (
+  id INTEGER NOT NULL PRIMARY KEY,
+  body TEXT NOT NULL,
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+
+);
+
+CREATE TABLE question_likes (
+  id INTEGER NOT NULL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+
+INSERT INTO
+  users(fname,lname)
+VALUES
+("Nathasha","Surendran"),
+("Jin", "Wu");
+
+INSERT INTO
+  questions(title, body, user_id)
+VALUES
+("Hello World", "We are the A/a student", 1),
+("Who am I?", "I am Jin the A/a student", 2);
+
+INSERT INTO
+  replies(body,question_id,user_id)
+VALUES
+("Good Question", 1, 1),
+("2nd answer", 1, 1),
+("Bad Question", 2, 2);
